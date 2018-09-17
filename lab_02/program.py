5cc0978abde33d25916959ad4c61f34334ff2c86
Madison <Madison@chen101.com>
2018-09-17 10:10:59 -0400

    program.py

---
+++ lab_02/program.py
@@ -0,0 +1,11 @@
+#vowel -> just add "ay" to end otherwise do first to end and add "ay"
+#Kush:3478587264
+def part_pig_latin(name):
+
+    if name[0]!= ("a" or "e" or "i" or "o" or "u"):
+        return name[1:] + name[0] + "ay"
+    else:
+        return name+"ay"
+    
+print(part_pig_latin("madison"))
+print(part_pig_latin("apple"))                                        
