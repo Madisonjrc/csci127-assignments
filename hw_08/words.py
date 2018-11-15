d426fd89e2b5392f07b9b421afb3fcb10e2e81b5
Madison <Madison@chen101.com>
2018-11-15 00:28:14 -0500

    words.py

---
+++ hw_08/words.py
@@ -0,0 +1,28 @@
+def clean(text):
+    result = ''
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for char in text.lower():
+        if char in alphabet:
+            result += char
+        else:
+            result += " "
+    return result
+
+def analyze(text):
+    text = clean(text)
+    l = text.split()
+    d = {}
+
+    for i in range(len(l)-1):
+        w = l[i]
+        d.setdefault(w, [])
+        n = l[i + 1] # next word
+        if n not in d[w]:
+            d[w].append(n)
+
+    return d
+
+f = open('moby-small.txt', 'r')
+text = f.read()
+d = analyze(text)
+print(d)
